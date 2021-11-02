2773b27e8b509eace927fd308039e483bda11d68
Joe H <38269467+bathrobe@users.noreply.github.com>
2021-11-02 17:33:58 -0400

    Update Map.jsx

--- web/component/Map.jsx
+++ web/component/Map.jsx
@@ -13,7 +13,7 @@ const Map = ({lat, long, type, locations}) => {
       style={type == "bigMap" ? { height: "100vh", width: "100vw" } : { height: "300px", width: "400px" }}       
     >
       <TileLayer
-        url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.MAPBOX_API_KEY}`}
+        url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.NEXT_PUBLIC_MAPBOX_API_KEY}`}
         attribution='Map data &copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors, <a href=&quot;https://creativecommons.org/licenses/by-sa/2.0/&quot;>CC-BY-SA</a>, Imagery &copy; <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>'
       />{type == "bigMap" ?
       locations.map(res => {return(
